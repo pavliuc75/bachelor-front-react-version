c26b17c075895c967dc70b1c3764b4f3afe85a68
Nicolai Pavliuc <pavliuc75@gmail.com>
2023-03-04 22:54:09 +0100

    added more dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useTranslation } from 'react-i18next';
 
 function App() {
+    const { t, i18n } = useTranslation();
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline text-dark-blue">
+          {t('createAnAccount')} {process.env.REACT_APP_URL}
+      </h1>
+        <FontAwesomeIcon icon={["fas", "coffee"]} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import './translations/i18n';
+
+library.add(fas);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
@@ -17,3 +23,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//todo: add body css
+//todo: util classes
+// i18n
