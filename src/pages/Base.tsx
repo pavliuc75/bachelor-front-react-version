d833cae02d8dcee2793945765da20c8cf54ca973
Nicolai Pavliuc <pavliuc75@gmail.com>
2023-03-06 22:28:31 +0100

    added common components

--- src/pages/Base.tsx
+++ src/pages/Base.tsx
@@ -1,17 +1,25 @@
-import { useSelector, useDispatch } from "react-redux";
-import type { RootState } from "../store";
-import { showSnackbar } from "../store/eventSlice";
+import {useSelector, useDispatch} from "react-redux";
+import type {RootState} from "../store";
+import {showSnackbar} from "../store/eventSlice";
+import CButtonPrimary from "../components/common/CButtonPrimary";
 
 function Base() {
-  const dispatch = useDispatch();
-  const isSnackbarShown = useSelector((state: RootState) => state.event.snackbar.isShown);
-  return (
-    <div>
-      <h1>Base</h1>
-      {isSnackbarShown.toString()}
-      <button onClick={() => dispatch(showSnackbar({ message: "a", type: "a" }))}>Show Snackbar</button>
-    </div>
-  );
+    const dispatch = useDispatch();
+    const isSnackbarShown = useSelector((state: RootState) => state.event.snackbar.isShown);
+
+    function kek() {
+        console.log("kek");
+    }
+
+    return (
+        <div>
+            <h1>Base</h1>
+            {isSnackbarShown.toString()}
+            <button onClick={() => dispatch(showSnackbar({message: "a", type: "a"}))}>Show Snackbar</button>
+            <br></br>
+            <CButtonPrimary onClick={() => kek()} iconStart={["fas", "coffee"]} text={"aa"}></CButtonPrimary>
+        </div>
+    );
 }
 
 export default Base;
