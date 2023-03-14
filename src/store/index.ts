c6de628b1296e6390a8c20d1b0bc6a42424c80df
Nicolai Pavliuc <pavliuc75@gmail.com>
2023-03-14 21:17:10 +0100

    added some store slices

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import eventReducer from "./eventSlice";
+import administratorManagementToolReducer from "./administratorManagementToolSlice";
+import businessManagementToolReducer from "./businessManagementToolSlice";
 
 export const store = configureStore({
   reducer: {
     event: eventReducer,
+    administratorManagementTool: administratorManagementToolReducer,
+    businessManagementTool: businessManagementToolReducer,
   },
 });
 
